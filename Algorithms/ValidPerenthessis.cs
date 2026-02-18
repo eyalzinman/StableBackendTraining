6fc32d925a3746fd098396c8f19d2e2659e1ad90
Einat Katzenell <einat@kdlaw.co.il>
2026-02-18 20:31:12 +0200

    my validParentheses

---
+++ Algorithms/ValidPerenthessis.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class ValidParentheses
+{
+    public bool IsValid(string s)
+    {
+        Stack<char> stack = new Stack<char>();
+        Dictionary<char, char> map = new Dictionary<char, char>()
+        {
+            { '(', ')' },
+            { '{', '}' },
+            { '[', ']' }
+        };
+
+        foreach (char c in s)
+        {
+            if (c == '(' || c == '{' || c == '[')
+            {
+                stack.Push(c);
+            }
+            else // closing
+            {
+                if (stack.Count == 0)
+                    return false;
+
+                char open = stack.Pop();
+                if (map[open] != c)
+                    return false;
+            }
+        }
+
+        return stack.Count == 0;
+    }
+}
